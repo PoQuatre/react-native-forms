43dd5a95fad13b80cda01d4329dd4d1715be98de
PoQuatre <leflem.mateo2002@gmail.com>
2022-04-07 13:46:29 +0200

    feat: add input validation

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 import { Input } from "./Input";
 import { Button } from "./Button";
+import { useForm, Controller } from "react-hook-form";
 
 const styles = StyleSheet.create({
   container: {
@@ -15,11 +16,74 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Input placeholder="Email" />
-      <Input placeholder="Password" />
-      <Button label="Login" />
+      <Controller
+        name="email"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: {
+            value: true,
+            message: "The email is required!",
+          },
+          pattern: {
+            value: /^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$/,
+            message: "A valid email must be entered!",
+          },
+        }}
+        render={({ field: { value, onChange } }) => (
+          <Input
+            autoComplete="email"
+            keyboardType="email-address"
+            placeholder="Email"
+            onChangeText={(text) => onChange(text)}
+            value={value}
+            error={errors.email?.message}
+          />
+        )}
+      />
+
+      <Controller
+        name="password"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: {
+            value: true,
+            message: "The password is required!",
+          },
+          minLength: {
+            value: 8,
+            message: "The password must be at least 8 characters long!",
+          },
+        }}
+        render={({ field: { value, onChange } }) => (
+          <Input
+            autoComplete="password"
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={(text) => onChange(text)}
+            value={value}
+            error={errors.password?.message}
+          />
+        )}
+      />
+
+      <Button
+        label="Login"
+        onPress={handleSubmit(onSubmit)}
+      />
     </SafeAreaView>
   );
 }
